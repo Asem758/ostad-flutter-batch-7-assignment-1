d9e37a94c49ac03b3247143119c3cfecb0419632
Ashrafur Rahman Asem <ashrafurasem@gmail.com>
2024-06-22 18:13:27 +0600

    Added Car Class

---
+++ bin/ostad_flutter_assignment_1.dart
@@ -0,0 +1,7 @@
+/*import 'package:ostad_flutter_assignment_1/ostad_flutter_assignment_1.dart' as ostad_flutter_assignment_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ostad_flutter_assignment_1.calculate()}!');
+}
+
+ */
